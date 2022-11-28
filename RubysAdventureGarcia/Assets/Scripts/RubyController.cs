903cb8d765e1242ced94beb7c9591f4a3c331835
707711 <707711@student.dusd.net>
2022-11-28 12:02:54 -0800

    New Scripts
    I added new prefabs along with adding and changing scripts

--- RubysAdventureGarcia/Assets/Scripts/RubyController.cs
+++ RubysAdventureGarcia/Assets/Scripts/RubyController.cs
@@ -7,7 +7,9 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    int currentHealth;
+
+    public int health { get { return currentHealth; } }
+    public int currentHealth;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -18,6 +20,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -36,7 +39,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
