fb60fe413f6010a3ca8a0d25d97f308434e144ed
707711 <707711@student.dusd.net>
2022-12-08 11:44:04 -0800

    Projectile
    I got projectiles to shoot out from Ruby

--- RubysAdventureGarcia/Assets/Scripts/Projectile.cs
+++ RubysAdventureGarcia/Assets/Scripts/Projectile.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        rigidbody2d = GetComponent<rigidbody2d>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     public void Launch(Vector2 direction, float force)
@@ -16,14 +16,24 @@ public class Projectile : MonoBehaviour
         rigidbody2d.AddForce(direction * force);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        //we also add a debug
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+
 }
